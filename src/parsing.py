ae17f05661fdd00f5732370d72e4fca01c04b3e5
NoahSchiro <noahschiro@rocketmail.com>
2023-01-16 11:07:42 -0500

    Able to parse ways

--- src/parsing.py
+++ src/parsing.py
@@ -1,28 +1,65 @@
 from xml.dom.minidom import parse
 
-# Parse into Document object
-data = parse("../data/selinsgrove.osm")
+def looking_at_tags(dom):
 
-# Gather all of the node objects
-nodes = data.getElementsByTagName("nodes")
+	# Gather all of the node objects
+	nodes = dom.getElementsByTagName("node")
 
-# For now, I just want to see what kind of "keys" are
-# in nodes, so we will create a set to gather them
-key_values = set()
+	# For now, I just want to see what kind of "keys" are
+	# in nodes, so we will create a set to gather them
+	key_values = set()
 
-# Go through all of the nodes
-for node in nodes:
+	# Go through all of the nodes
+	for node in nodes:
 
-	# Go through all of the tags in the nodes
-	tags = node.getElementsByTagName("tag")
+		# Go through all of the tags in the nodes
+		tags = node.getElementsByTagName("tag")
 
-	for tag in tags:
+		for tag in tags:
 
-		# Get the key and add it to the set
-		key = tag.getAttribute("k")
+			key_values.add(tag.getAttribute("k"))
 
-		key_values.add(key)
+	# Print the set of keys
+	for key in key_values:
+		print(key)
 
-# Print the set of keys
-for key in key_values:
-	print(key)
+def looking_at_ways(dom):
+
+	ways = dom.getElementsByTagName("way")
+
+	nodes = dom.getElementsByTagName("node")
+
+	# For now, let's look at the connection of one way.
+	# The tag "nd" will generate the nodes that this way is connected to
+	node_list = ways[0].getElementsByTagName("nd")
+
+	# Collect the nodes that this way is associated with
+	node_ids = []
+	for i in range(node_list.length):
+		node_ids.append(node_list.item(i).getAttribute("ref"))
+
+	# Now, loop through all of the 
+	# nodes and find the ones we care about
+	nodes_connected = []
+	for node in nodes:
+		for i in node_ids:
+			if node.getAttribute("id") == i:
+				nodes_connected.append(node)
+
+	# Now we want to fetch 
+	# the lat and lon of each of these nodes
+	lat_lon = []
+	for node in nodes_connected:
+		lat = float(node.getAttribute("lat"))
+		lon = float(node.getAttribute("lon"))
+		lat_lon.append((lat,lon))
+
+	for i in lat_lon:
+		print(i)
+
+
+
+if __name__ == "__main__":
+
+	data = parse("../data/selinsgrove.osm")
+	looking_at_ways(data)
